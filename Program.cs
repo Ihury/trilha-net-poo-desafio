0b804bcfe5f6c264bdbca1ce709478a8e93f8ed0
Ihury <ihurykc@gmail.com>
2023-10-04 18:40:14 -0300

    Desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia celularNokia = new("1234", "Nokia U", "1234", 256);
+Iphone celularIphone = new("5678", "Iphone 8", "5678", 512);
+
+celularNokia.InstalarAplicativo("WhatsApp");
+celularIphone.InstalarAplicativo("Instagram");
